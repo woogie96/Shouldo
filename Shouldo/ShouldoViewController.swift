fffe811f9b47f6bea082f612650d37418c774162
yunuki <woogieyun@gmail.com>
2020-06-09 14:29:04 +0900

    test2

--- Shouldo/ShouldoViewController.swift
+++ Shouldo/ShouldoViewController.swift
@@ -22,7 +22,6 @@ class ShouldoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("test")
         addTextField.delegate = self
         updateFirstUI()
         shouldoList = DataManager.shared.fetchShouldo(dayOfTheWeek: dayOfTheWeek)
